ea3d145b31315ddab0976bafbdd57cef22d18d8f
Deszirae1 <166359401+Deszirae1@users.noreply.github.com>
2024-11-12 23:15:30 -0800

    islogged in fixd

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,6 +1,6 @@
 const { findUserWithToken } = require("../db");
 
-const isLoggedIn = async (req, next) => {
+const isLoggedIn = async (req, res, next) => {
   try {
     req.user = await findUserWithToken(req.headers.authorization);
     next();
