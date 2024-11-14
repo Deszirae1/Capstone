6048ea6dd56ca9157a1f29d1e763a94dc6d5954b
Deszirae1 <166359401+Deszirae1@users.noreply.github.com>
2024-11-14 10:37:56 -0800

    changes

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const secretKey = process.env.TOKEN_SECRET; // Use environment variable
+const secretKey = process.env.TOKEN_SECRET; 
+
+console.log('TOKEN_SECRET:', secretKey); 
 
 const { fetchReviews, createReview, editReview, deleteReview } = require("../db");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(403).json({ status: 'error', message: 'Authorization token required' });
-  }
-
-  try {
-    const decoded = jwt.verify(token.split(' ')[1], secretKey); // Assuming the token is in the format "Bearer <token>"
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json({ status: 'error', message: 'Invalid token' });
-  }
-};
-
-router.get("/", authMiddleware, async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     res.send(await fetchReviews());
   } catch (ex) {
@@ -28,15 +15,18 @@ router.get("/", authMiddleware, async (req, res, next) => {
   }
 });
 
-router.post("/create", authMiddleware, async (req, res, next) => {
+
+
+router.post("/create", async (req, res, next) => {
   try {
     res.send(await createReview(req.body));
   } catch (ex) {
-    next(ex); 
+      next(ex); 
   }
 });
 
-router.put("/:review_id", authMiddleware, async (req, res, next) => {
+
+router.put("/:review_id", async (req, res, next) => {
   try {
     const { review_id } = req.params;
     const { description, rating } = req.body;
@@ -48,7 +38,7 @@ router.put("/:review_id", authMiddleware, async (req, res, next) => {
   }
 });
 
-router.delete("/:review_id", authMiddleware, async (req, res, next) => {
+router.delete("/:review_id", async (req, res, next) => {
   try {
     const { review_id } = req.params;
     
@@ -58,5 +48,9 @@ router.delete("/:review_id", authMiddleware, async (req, res, next) => {
     next(ex);
   }
 });
+
+
+
+
 
 module.exports = router;
